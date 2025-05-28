a127660742d7e4957e4bcf61e9387ad9a87126c9
Alper Kara <alperkara247@gmail.com>
2025-05-29 00:47:40 +0300

    Initial Commit

---
+++ WallpaperColorGenerator/WallpaperColorGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpaperColorGeneratorApp.swift
+//  WallpaperColorGenerator
+//
+//  Created by Alper Kara on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct WallpaperColorGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
